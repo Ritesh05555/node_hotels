a0f2186a32890fbd546ca40c79f96b838bb9315a
RITESH SAHANI <shubamsahani03@gmail.com>
2025-02-14 19:32:31 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURL='mongodb://localhost:27017/mydatabase' //replace "mydatabase" with your database name
+ // const mongoURL=process.env.MONGODB_URL_LOCAL; //replace "mydatabase" with your database name
+const mongoURL=process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL,{
     useNewUrlParser: true,
--- server.js
+++ server.js
@@ -52,9 +52,12 @@
 const express = require('express');
 const app = express();
 const db = require('./db'); // Ensure db connection is properly set up
+require('dotenv').config();
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // Middleware for parsing JSON request bodies
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
     res.send('Welcome to my hotel... How can I help you?');
@@ -66,6 +69,7 @@ const menuItemRoutes = require('./routes/menuItemRoutes')
 app.use('/person',personRoutes);
 app.use('/menu',menuItemRoutes);
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
